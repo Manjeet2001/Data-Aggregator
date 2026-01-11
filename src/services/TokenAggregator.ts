6dd450a24dc276cf02c91d5690ebcd340c57ecae
Manjeet2001 <manjeetydv16@gmail.com>
2026-01-11 11:24:28 +0530

    implemented token aggregation service

---
+++ src/services/TokenAggregator.ts
@@ -0,0 +1,47 @@
+import { EventEmitter } from 'events';
+import { DexScreenerClient } from './DexScreenerClient';
+import { JupiterClient } from './JupiterClient';
+import { CacheService } from './CacheService';
+import { TokenData } from '../types';
+
+export class TokenAggregator extends EventEmitter {
+    private dexClient: DexScreenerClient;
+    private jupClient: JupiterClient;
+    private cache: CacheService;
+    private isRunning: boolean = false;
+
+    constructor() {
+        super();
+        this.dexClient = new DexScreenerClient();
+        this.jupClient = new JupiterClient();
+        this.cache = new CacheService();
+    }
+
+    async start(intervalMs: number = 30000) {
+        if (this.isRunning) return;
+        this.isRunning = true;
+        console.log('Aggregator started...');
+
+
+        await this.aggregate();
+
+
+        setInterval(() => {
+            this.aggregate().catch(console.error);
+        }, intervalMs);
+    }
+
+    async aggregate() {
+        console.log('Fetching data...');
+
+        const dexTokens = await this.dexClient.searchTokens('SOL');
+
+
+        for (const token of dexTokens) {
+            await this.cache.saveToken(token);
+        }
+
+        this.emit('update', dexTokens);
+        console.log(`Aggregated ${dexTokens.length} tokens.`);
+    }
+}
